d15e39306b213be418b890d565d26a05eca8fe33
Sangam Ghimire <1sangamghimire1@gmail.com>
2023-07-11 16:28:36 +0545

    fixed alignments in TextureManager.cpp

--- main/TextureManager.cpp
+++ main/TextureManager.cpp
@@ -5,16 +5,13 @@
 
 
 bool TextureManager::load(std::string fileName, std::string id, SDL_Renderer* pRenderer) {
-	
 	SDL_Surface* pTempSurface = IMG_Load(fileName.c_str());
-	
 	if (pTempSurface == 0)
 	{
 		return false;
 	}
 	SDL_Texture* pTexture = SDL_CreateTextureFromSurface(pRenderer, pTempSurface);
 	SDL_CreateTextureFromSurface(pRenderer, pTempSurface);
-
 	SDL_FreeSurface(pTempSurface);
 
 	//check if okkkk
@@ -27,7 +24,6 @@ bool TextureManager::load(std::string fileName, std::string id, SDL_Renderer* pR
 }
 
 void TextureManager::draw(std::string id, int x, int y, int width, int height, SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE) {
-
 	SDL_Rect srcRect;
 	SDL_Rect destRect;
 
@@ -39,7 +35,6 @@ void TextureManager::draw(std::string id, int x, int y, int width, int height, S
 	destRect.y = y;
 
 	SDL_RenderCopyEx(pRenderer, m_textureMap[id], &srcRect, &destRect, 0, 0, flip); //gets the object we want to draw
-
 }
 
 void TextureManager::drawFrame(std::string id, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer* pRenderer, SDL_RendererFlip flip) {
